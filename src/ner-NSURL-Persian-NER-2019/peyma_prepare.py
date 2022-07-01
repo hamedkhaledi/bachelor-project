11436e5336effe2caaf72ac2a787a0a70e0c1f8f
hamed khaledi <Khaledihkh@gmail.com>
2022-07-01 07:58:38 +0430

    add colab codes

---
+++ src/ner-NSURL-Persian-NER-2019/peyma_prepare.py
@@ -0,0 +1,29 @@
+from flair.data import Sentence
+from flair.models import SequenceTagger
+from flair.data import Corpus
+from flair.datasets import ColumnCorpus
+from pathlib import Path
+
+# define columns
+columns = {0: 'text', 1: 'ner'}
+data_folder = "./data/ner-NSURL-Persian-NER-2019/"
+res_text = "./result/ner-NSURL-Persian-NER-2019/res-peyma.txt"
+corpus: Corpus = ColumnCorpus(data_folder, columns,
+                              train_file='train.txt',
+                              test_file='test_predictions_clean.txt',
+                              dev_file='valid.txt')
+# load the model you trained
+print(Path(data_folder + 'model/final-model.pt').exists())
+model = SequenceTagger.load(data_folder + 'model/final-model.pt')
+# create example sentence
+sentence = Sentence(' من  نیویورک رو دوست دارم')
+
+# predict tags and print
+model.predict(sentence)
+
+print(sentence.to_tagged_string())
+
+result = model.evaluate(corpus.test, gold_label_type="ner", mini_batch_size=4, out_path=f"predictions.txt")
+print(result)
+with open(res_text, "w") as file1:
+    file1.write(str(result))
