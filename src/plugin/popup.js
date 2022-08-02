1e2854b9954ef6e5c779480c7e48e2766f605a74
hamed khaledi <Khaledihkh@gmail.com>
2022-08-03 00:02:50 +0430

    update plugin front

--- src/plugin/popup.js
+++ src/plugin/popup.js
@@ -7,15 +7,26 @@ window.onload = function () {
     for (let i = 0; i < elements.length; i++) {
         elements[i].addEventListener("click", changeStyle);
     }
+
+    var checkBox = document.getElementById("selection");
+    checkBox.addEventListener("click", changeSelection);
+    checkBox.checked = "true";
 }
 const colors = {
     "PER": "#aadb51",
+    "ADJ": "#aadb51",
     "ORG": "#ffe05d",
+    "V": "#ffe05d",
     "LOC": "lightpink",
+    "N": "lightpink",
     "DAT": "lightblue",
+    "P": "lightblue",
     "TIM": "crimson",
+    "DELM": "crimson",
     "PCT": "lightsalmon",
+    "ADV": "lightsalmon",
     "MON": "mediumpurple",
+    "CON": "mediumpurple",
 
 }
 
@@ -29,6 +40,35 @@ function changeStyle(event) {
             elements[i].style.backgroundColor = "rgb(0,0,0,0)";
         }
     }
+}
+
+function changeSelection(event) {
+    var checkBox = document.getElementById("selection");
+    var textSpan = document.getElementById("statusText");
+    var nerButtons = document.getElementById("nerButtons");
+    var posButtons = document.getElementById("posButtons");
+    if (checkBox.checked === true) {
+        textSpan.style.color = "limeGreen";
+        textSpan.innerHTML = "NER";
+        posButtons.classList.add('visuallyhidden');
+        posButtons.classList.add('hidden');
+        nerButtons.classList.remove('hidden');
+        setTimeout(function () {
+            nerButtons.classList.remove('visuallyhidden');
+        }, 20);
+
+
+    } else {
+        textSpan.style.color = "darkRed";
+        textSpan.innerHTML = "POS";
+        posButtons.classList.remove('hidden');
+        nerButtons.classList.add('visuallyhidden');
+        nerButtons.classList.add('hidden');
+        setTimeout(function () {
+            posButtons.classList.remove('visuallyhidden');
+        }, 20);
+    }
+
 }
 
 function outHtml(text) {
